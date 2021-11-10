b7cf8815948ee3acef798080d015257c86f71cca
Jeri Ramadhan <jeriramadhan25@gmail.com>
2021-11-10 11:27:06 +0700

    tambah proses edit modal

--- tbl_kategori.php
+++ tbl_kategori.php
@@ -27,7 +27,7 @@ include 'header.php';
     <div class="container-fluid">
       <!-- Small boxes (Stat box) -->
       <div class="row">
-        <div class="col-md-8 ml-5">
+        <div class="col-md-12">
           <div class="card">
             <div class="card-header bg-secondary text-white">
               DATA KATEGORI
